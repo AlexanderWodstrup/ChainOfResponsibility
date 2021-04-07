cfb68ef46d0dd9120c4f035b0a4e9fa34861b286
augusthjerrild <augusthjerrild@gmail.com>
2021-04-07 20:43:07 +0200

    help

--- ChainOfResponsibility/BaseHandler.cs
+++ ChainOfResponsibility/BaseHandler.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ChainOfResponsibility
 {
-    abstract class BaseHandler
+    public abstract class BaseHandler
     {
 
         private BaseHandler _nextHandler;
